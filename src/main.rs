c87891126457f97e5b0c1dce0d7df0130b06f918
twallie <thomaswallsmith@gmail.com>
2023-12-31 07:41:03 -0500

    feat: ask loop and input check

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,51 @@
-use std::fs;
+use std::{fs, collections::HashSet};
 use rand::{thread_rng, Rng};
+use std::io::stdin;
 
 fn main() {
     let words = read_words();
-    let random_word = get_random_word(&words);
-    println!("{random_word}");
+    let word = get_random_word(&words);
+    let hashset = build_word_hashset(word);
+
+    loop {
+        println!("Guess a letter:");
+
+        let guess = match read_in_alphabetic_character() {
+            Ok(v) => v,
+            Err(_) => continue
+        };
+
+        println!("That do be what I am looking for fr fr");
+    }
+}
+
+fn read_in_alphabetic_character() -> Result<char, &'static str> {
+    let mut guess: String = String::new();
+    let stdin_result = stdin()
+        .read_line(&mut guess);
+
+    match stdin_result {
+        Ok(_) => (),
+        Err(_) => return Err("Error reading from stdin")
+    };
+
+    let char: char = match guess.trim().parse() {
+        Ok(v) => v,
+        Err(_) => return Err("Error converting user input, likely not a char"),
+    };
+
+    match char.is_alphabetic() {
+        true => return Ok(char),
+        false => return Err("Input was not alphabetic")
+    }
+}
+
+fn build_word_hashset(word: &String) -> HashSet<char> {
+    let mut hashset: HashSet<char> = HashSet::new();
+    for char in word.chars() {
+        hashset.insert(char);
+    }
+    return hashset;
 }
 
 fn get_random_word(words: &Vec<String>) -> &String {
